5c1f2caeed948327d209fd2aa348282b006bfc74
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-04-28 19:21:17 +0530

    Add auth state change

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -10,10 +11,21 @@ export class LoginComponent implements OnInit {
     email: '',
     password: '',
   };
+  showAlert = false;
+  alertMessage = 'Please wait! We are logging you in.';
+  alertColor = 'blue';
+  inSubmission = false;
 
-  constructor() {}
+  constructor(private auth: AngularFireAuth) {}
 
   ngOnInit(): void {}
 
-  login() {}
+  async login() {
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email,
+        this.credentials.password
+      );
+    } catch (err) {}
+  }
 }
